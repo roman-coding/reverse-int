cd53e5edb421641878113eb9dc54d3b85924eceb
roman-coding <mbizgle@gmail.com>
2023-01-12 18:45:41 +0300

    feat: add complete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newNum = null;
+    Math.sign(n) === -1 ? (newNum = n * -1) : (newNum = n);
+    return Number(String(newNum).split("").reverse().join(""));
+};
